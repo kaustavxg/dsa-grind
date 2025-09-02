debcaae9aa750dc700bf800ab8d127482b246bbc
kaustavgupta <kaustavgupta2311@gmail.com>
2025-09-03 02:35:12 +0530

    addd 03_sorted_array and Q26 of Leetcode

--- 000 - dsa-problems/01 - arrays/03_check_array_is_sorted.cpp
+++ 000 - dsa-problems/01 - arrays/03_check_array_is_sorted.cpp
@@ -1,9 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-
-
-
 int main(){
     int n;
     cout << "enter the length: ";
@@ -14,6 +11,4 @@ int main(){
     for(int i = 0; i < n; i++){
         cin >> arr[i];
     }
-
-
 }
