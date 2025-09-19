231a68fe93a9fe335cc1efeb75f9cfe8c0857356
kaustavgupta <kaustavgupta2311@gmail.com>
2025-09-19 16:27:30 +0530

    Q53 - Maximum subarray

--- 000 - dsa-problems/01 - arrays/02-Medium/04 - Kadane's Algorithm/Q53-LC_maximum_subarray.cpp
+++ 000 - dsa-problems/01 - arrays/02-Medium/04 - Kadane's Algorithm/Q53-LC_maximum_subarray.cpp
@@ -1,46 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-/*
-! Q53 - Maximum Subarray
-
-Given an integer array nums, find the subarray with the largest sum, and return its sum.
-
-Example 1:
-
-Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
-Output: 6
-Explanation: The subarray [4,-1,2,1] has the largest sum 6.
-Example 2:
-
-Input: nums = [1]
-Output: 1
-Explanation: The subarray [1] has the largest sum 1.
-Example 3:
-
-Input: nums = [5,4,-1,7,8]
-Output: 23
-Explanation: The subarray [5,4,-1,7,8] has the largest sum 23.
-
-! Intuition:
-#Brute Force:
-1. try out all the sub-arrays
-2. return the max sub array
-3. why worst? - gives TC: O(n^3) 
-4. gives TLE
-
-# Better Approach
-1.
-2. TC: O(n^2)
-
-# Optimal Approach - Kadane Algo
-1.
-2.
-3.
-
-
-*/
-
 //# brute force approach
 int brute_kadane(int arr[], int n){
     int maxi = INT_MIN;
